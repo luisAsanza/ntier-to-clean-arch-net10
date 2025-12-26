77d9a7af2caaddcb5afb0f4d0873f476d9277a80
Luis Asanza <luisasanza@hotmail.com>
2025-12-26 10:01:59 -0300

    chore: Replace api with mvc project and updated references

---
+++ src/CleanCRUDSolution.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CleanCRUDSolution.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
